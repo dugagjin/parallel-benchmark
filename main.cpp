c9dd7f7156409aeeab2c2fdc654cad940e43cdd0
Dugagjin Lashi <lashi.dugagjin@gmail.com>
2016-12-22 11:22:56 +0100

    better code

--- main.cpp
+++ main.cpp
@@ -1,39 +1,44 @@
 #include <iostream>
+#include <stdlib.h>
 #include <time.h>
 #include <chrono>
+
+#ifdef _WIN32
+	#define WINPAUSE system("pause")
+#endif
 
 #define arrayLength 1000000
 #define maxRandomValue 1000
 
 using namespace std;
-using namespace std::chrono;
+using namespace chrono;
 
-auto initializeArray = [](){
-    //srand(time(NULL));
-    auto* array = new int [arrayLength];
-    for (auto i = 0; i < arrayLength; i++){
-        array[i] = rand() % maxRandomValue + 1;
-    }
-    return array;
+auto initializeArray = []() {
+	auto* array = new int[arrayLength];
+	for (auto i = 0; i < arrayLength; i++) {
+		array[i] = rand() % maxRandomValue + 1;
+	}
+	return array;
 };
 
-auto sequentialSum = [](auto array){
-    auto sum = 0;
-    for (auto i = 0; i < arrayLength; i++){
-        sum += array[i];
-    }
-    return sum;
+auto sequentialSum = [](auto array) {
+	auto sum = 0;
+	for (auto i = 0; i < arrayLength; i++) {
+		sum += array[i];
+	}
+	return sum;
 };
 
-auto calculateTime = [](auto function, auto array){
-    steady_clock::time_point begin = steady_clock::now();
-    auto sum = function(array);
-    steady_clock::time_point end = steady_clock::now();
-    cout << "required time: " << chrono::duration_cast<chrono::microseconds>(end - begin).count() << " µs\nsum: " <<  sum <<endl;
+auto calculateTime = [](auto function, auto array) {
+	auto begin = steady_clock::now();
+	auto sum = function(array);
+	auto end = steady_clock::now();
+	cout << "required time: " << duration_cast<microseconds>(end - begin).count() << " us\nsum: " << sum << endl;
 };
 
-int main (){
-    auto* array = initializeArray();
-    calculateTime(sequentialSum, array);
-    return 0;
+int main() {
+	auto* array = initializeArray();
+	calculateTime(sequentialSum, array);
+	WINPAUSE;
+	return 0;
 }

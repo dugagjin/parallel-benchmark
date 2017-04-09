5735812709c56c0d8a18d115eaa9922f4e777bf8
Dugagjin Lashi <lashi.dugagjin@gmail.com>
2017-04-09 16:45:48 +0200

    fixed threads solution

--- main.cpp
+++ main.cpp
@@ -1,49 +1,46 @@
 #include <iostream>
 #include <chrono>
 #include <string>
-#include <thread>
+#include <future>
 #include <vector>
-#include <cassert>
+#include <numeric>
 
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
+#include <list>
 #pragma comment(lib, "user32.lib")
 
 #ifdef _WIN32
 #define WINPAUSE system("pause")
 #endif
 
-#define arrayLength 65536
-#define maxRandomValue 10
-#define nameVariable(a) #a
-
 using namespace std;
 using namespace chrono;
 
-
-auto getSystemInfo() -> void {
+auto printSystemInfo = [] {
 	try {
 		LARGE_INTEGER lpFrequency;
 		QueryPerformanceFrequency(&lpFrequency);
 		SYSTEM_INFO siSysInfo;
 		GetSystemInfo(&siSysInfo);
-		auto CPUSpeed = float(lpFrequency.QuadPart) / 1000000;
-		cout << siSysInfo.dwNumberOfProcessors << " cores at about " << CPUSpeed << " Ghz" << endl << endl;
-	} catch (int e) {
-		cout << "Could not get systeminfo. Exception #" << e << '\n';
+		cout << siSysInfo.dwNumberOfProcessors << " cores at about " << float(lpFrequency.QuadPart) / 1'000'000 << " Ghz" << endl;
+	} catch (exception const& e) {
+		cout << "Error: " << e.what() << '\n';
 	}
 };
 
-auto initializeArray() -> auto* {
-	auto* array = new int[arrayLength];
+int arrayLength;
+auto initializeRandomArray= [](auto length, auto maxRandomValue) -> auto* {
+	arrayLength = length;
+	auto array = new int[arrayLength];
 	for (auto i = 0; i < arrayLength; i++) {
 		array[i] = rand() % maxRandomValue + 1;
 	}
 	return array;
 };
 
-auto calculateTime = [](auto name, auto function, auto &array) {
+auto calculateTime = [](auto name, auto &function, auto &array) {
 	auto begin = steady_clock::now();
 	auto sum = function(array);
 	auto end = steady_clock::now();
@@ -51,89 +48,87 @@ auto calculateTime = [](auto name, auto function, auto &array) {
 	cout << " The sum is: " << sum << endl;
 };
 
-auto sequentialSum = [](auto &array) {
-	auto sum = 0;
-	for (auto i = 0; i < arrayLength; i++) {
-		sum += array[i];
+class Methods {
+public:
+	static auto Sum() {
+		vector<pair<string, function<int(int* &)>>> functions = {
+			make_pair("default", [](auto &array) {
+				auto sum = 0;
+				for (auto i = 0; i < arrayLength; i++) {
+					sum += array[i];
+				}
+				return sum;
+			}),
+			make_pair("auto parallel", [](auto &array) {
+				auto sum = 0;
+				#pragma loop(ivdep)
+				#pragma loop(hint_parallel(4))
+				for (auto i = 0; i < arrayLength; i++) {
+					sum += array[i];
+				}
+				return sum;
+			}),
+			make_pair("no vector", [](auto &array) {
+				auto sum = 0;
+				#pragma loop(ivdep)
+				#pragma loop(no_vector)
+				for (auto i = 0; i < arrayLength; i++) {
+					sum += array[i];
+				}
+				return sum;
+			}),
+			make_pair("openMP", [](auto &array) {
+				auto sum = 0;
+				#pragma omp parallel for reduction(+:sum)
+				for (auto i = 0; i < arrayLength; i++) {
+					sum += array[i];
+				}
+				return sum;
+			}),
+			make_pair("SIMD", [](auto array) {
+				/* array needs to be 16 byte aligned, Thats why it does not work */
+				const auto vk0 = _mm_set1_epi8(0);
+				const auto vk1 = _mm_set1_epi16(1);
+				auto vsum = _mm_set1_epi32(0);
+				auto n = arrayLength / 16;
+				for (auto i = 0; i < n; i += 16) {
+					__m128i v = _mm_loadu_si128(reinterpret_cast<__m128i*>(&array[i]));
+					auto vl = _mm_unpacklo_epi8(v, vk0);
+					auto vh = _mm_unpackhi_epi8(v, vk0);
+					vsum = _mm_add_epi32(vsum, _mm_madd_epi16(vl, vk1));
+					vsum = _mm_add_epi32(vsum, _mm_madd_epi16(vh, vk1));
+				}
+				vsum = _mm_add_epi32(vsum, _mm_srli_si128(vsum, 8));
+				vsum = _mm_add_epi32(vsum, _mm_srli_si128(vsum, 4));
+				return  _mm_cvtsi128_si32(vsum);
+			}),
+			make_pair("threads", [](auto &array) {
+				auto sum = 0, numberOfThreads = 8, elementsPerThread = (arrayLength / numberOfThreads);
+				auto accumulator = [](auto* array, auto max) {
+					auto sum = 0;
+					for (auto i = 0; i < max; i++) {
+						sum += array[i];
+					}
+					return sum;
+				};
+				vector<future<int>> threads;
+				for(auto i = 0; i < numberOfThreads; i++) {
+					threads.push_back(async(launch::async, accumulator, array + i * elementsPerThread, elementsPerThread));
+					sum += threads.at(i).get();
+				}
+				return sum;
+			}),
+		};
+		return functions;
 	}
-	return sum;
-};
-
-auto autoParallelSum = [](auto &array) {
-	auto sum = 0;
-#pragma loop(ivdep)
-#pragma loop(hint_parallel(4))
-	for (auto i = 0; i < arrayLength; i++) {
-		sum += array[i];
-	}
-	return sum;
-};
-
-auto noVectorSum = [](auto &array) {
-	auto sum = 0;
-#pragma loop(ivdep)
-#pragma loop(no_vector)
-	for (auto i = 0; i < arrayLength; i++) {
-		sum += array[i];
-	}
-	return sum;
-};
-
-auto openMPSum = [](auto &array) {
-	auto sum = 0;
-#pragma omp parallel for reduction(+:sum)
-	for (auto i = 0; i < arrayLength; i++) {
-		sum += array[i];
-	}
-	return sum;
-};
-
-auto SIMDSum = [](auto &array) {
-	auto sum = 0.0f;
-	/*
-	auto n = 16;
-	auto vsum = _mm_set1_ps(0.0f);
-	assert((n & 3) == 0);
-	assert(((uintptr_t)(array) & 15) == 0);
-	for (auto i = 0; i < n; i += 4){
-	__m128 v = _mm_load_ps(&array[i]);
-	vsum = _mm_add_ps(vsum, v);
-	}
-	vsum = _mm_hadd_ps(vsum, vsum);
-	vsum = _mm_hadd_ps(vsum, vsum);
-	_mm_store_ss(&sum, vsum);
-	*/
-	return static_cast<int>(sum);
-};
-
-auto multiThreadedSum = [](auto &array) {
-	const auto numberOfThreads = 2;
-	const auto elementsPerThread = arrayLength / numberOfThreads;
-	auto sum = 0;
-	auto threadSum = [&sum](auto* arrayThread, auto max) -> void {
-		for (auto i = 0; i < max; i++) {
-			sum += arrayThread[i];
-		}
-	};
-	vector<thread*> threads;
-	for (auto i = 0; i < numberOfThreads; i++) {
-		threads.push_back(new thread(threadSum, array + i * elementsPerThread, elementsPerThread));
-	}
-	for (auto t : threads) {
-		t->join();
-		delete t;
-	}
-	return sum;
 };
 
 int main() {
-	getSystemInfo();
-	auto* array = initializeArray();
-	calculateTime(nameVariable(sequentialSum), sequentialSum, array);
-	calculateTime(nameVariable(autoParallelSum), autoParallelSum, array);
-	calculateTime(nameVariable(autoVectorSum), noVectorSum, array);
-	calculateTime(nameVariable(openMPSum), openMPSum, array);
-	calculateTime(nameVariable(multiThreadedSum), multiThreadedSum, array);
+	printSystemInfo();	
+	auto* arrayOfRandomNumbers = initializeRandomArray(262'144, 1);
+	for(auto &f : Methods::Sum()) {
+		calculateTime(f.first, f.second, arrayOfRandomNumbers);
+	}
 	WINPAUSE;
 	return 0;
 }

176f2c327be81c8694dc56b7e4241c00fb47ef26
Dugagjin Lashi <lashi.dugagjin@gmail.com>
2017-06-19 18:55:31 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,24 +6,25 @@
 #include <map>
 #include <numeric>
 #include <limits>
+#include <fstream>
 
 #include <stdlib.h>
+#define NOMINMAX
 #include <windows.h>
-
-#pragma comment(lib, "user32.lib")
-#define WINPAUSE system("pause")
+#undef NOMINMAX
 
 #include <CL/cl.hpp>
+#include "json.hpp"
 
+using json = nlohmann::json;
 using namespace std;
 using namespace chrono;
-
-
 
 auto add = [](auto name) {
 	map<string, function<unique_ptr<float[]>(float* &, float* &, int)>> methods;
 	methods["classic"] = [](auto &array1, auto &array2, auto arrayLength) {
 		unique_ptr<float[]> arrayOut(new float[arrayLength]);
+		#pragma loop(no_vector)	
 		for (auto i = 0; i < arrayLength; i++) {
 			arrayOut[i] = array1[i] + array2[i];
 		}
@@ -33,6 +34,7 @@ auto add = [](auto name) {
 		unique_ptr<float[]> arrayOut(new float[arrayLength]);
 		#pragma loop(ivdep)
 		#pragma loop(hint_parallel(4))
+		#pragma loop(no_vector)	
 		for (auto i = 0; i < arrayLength; i++) {
 			arrayOut[i] = array1[i] + array2[i];
 		}
@@ -40,8 +42,6 @@ auto add = [](auto name) {
 	};
 	methods["autovector"] = [](auto &array1, auto &array2, auto arrayLength) {
 		unique_ptr<float[]> arrayOut(new float[arrayLength]);
-		#pragma loop(ivdep)
-		#pragma loop(no_vector)
 		for (auto i = 0; i < arrayLength; i++) {
 			arrayOut[i] = array1[i] + array2[i];
 		}
@@ -49,7 +49,7 @@ auto add = [](auto name) {
 	};
 	methods["openmp"] = [](auto &array1, auto &array2, auto arrayLength) {
 		unique_ptr<float[]> arrayOut(new float[arrayLength]);
-		#pragma omp parallel for 
+		#pragma omp parallel for
 		for (auto i = 0; i < arrayLength; i++) {
 			arrayOut[i] = array1[i] + array2[i];
 		}
@@ -58,6 +58,7 @@ auto add = [](auto name) {
 	methods["simd"] = [](auto &array1, auto &array2, auto arrayLength) {
 		unique_ptr<float[]> arrayOut(new float[arrayLength]);
 		const int aligned = arrayLength - arrayLength % 4;
+		#pragma loop(no_vector)	
 		for (auto i = 0; i < aligned; i += 4) {
 			_mm_storeu_ps(&arrayOut[i], _mm_add_ps(_mm_loadu_ps(&array1[i]), _mm_loadu_ps(&array2[i])));
 		}
@@ -70,6 +71,7 @@ auto add = [](auto name) {
 		unique_ptr<float[]> arrayOut(new float[arrayLength]);
 		auto numberOfThreads = 4, elementsPerThread = (arrayLength / numberOfThreads);
 		auto add = [](auto* array1, auto* array2, auto* array3, auto max) {
+			#pragma loop(no_vector)	
 			for (auto i = 0; i < max; i++) {
 				array3[i] = array1[i] + array2[i];
 			}
@@ -95,18 +97,10 @@ auto add = [](auto name) {
 	methods["openCL"] = [](auto &array1, auto &array2, auto arrayLength) {
 		vector<cl::Platform> all_platforms;
 		cl::Platform::get(&all_platforms);
-		if (all_platforms.size() == 0) {
-			cout << " No platforms found. Check OpenCL installation!\n";
-			exit(1);
-		}
 		cl::Platform default_platform = all_platforms[0];
 
 		vector<cl::Device> all_devices;
 		default_platform.getDevices(CL_DEVICE_TYPE_ALL, &all_devices);
-		if (all_devices.size() == 0) {
-			cout << " No devices found. Check OpenCL installation!\n";
-			exit(1);
-		}
 		cl::Device default_device = all_devices[0];
 		cl::Context context({ default_device });
 		cl::Program::Sources sources;
@@ -143,63 +137,6 @@ auto add = [](auto name) {
 		queue.enqueueReadBuffer(buffer_C, CL_TRUE, 0, sizeof(float) * arrayLength, arrayOut.get());
 		return arrayOut;
 	};
-	methods["cuda"] = [](auto &array1, auto &array2, auto arrayLength) {
-		float* h_A = (float*)malloc(size);
-		float* h_B = (float*)malloc(size);
-		float* h_C = (float*)malloc(size);
-
-		// declare device vectors in the device (GPU) memory
-		float *d_A, *d_B, *d_C;
-
-		// initialize input vectors
-		for (int i = 0; i<n_el; i++) {
-			h_A[i] = sin(i);
-			h_B[i] = cos(i);
-		}
-
-		// allocate device vectors in the device (GPU) memory
-		cudaMalloc(&d_A, size);
-		cudaMalloc(&d_B, size);
-		cudaMalloc(&d_C, size);
-
-		// copy input vectors from the host (CPU) memory to the device (GPU) memory
-		cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
-		cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
-
-		// call kernel function
-		sum(d_A, d_B, d_C, n_el);
-
-		// copy the output (results) vector from the device (GPU) memory to the host (CPU) memory
-		cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
-		// free device memory
-		cudaFree(d_A);
-		cudaFree(d_B);
-		cudaFree(d_C);
-
-		// compute the cumulative error
-		double err = 0;
-		for (int i = 0; i<n_el; i++) {
-			double diff = double((h_A[i] + h_B[i]) - h_C[i]);
-			err += diff*diff;
-			// print results for manual checking.
-			std::cout << "A+B: " << h_A[i] + h_B[i] << "\n";
-			std::cout << "C: " << h_C[i] << "\n";
-		}
-		err = sqrt(err);
-		std::cout << "err: " << err << "\n";
-
-		// free host memory
-		delete[] h_A;
-		delete[] h_B;
-		delete[] h_C;
-
-		//return cudaDeviceSynchronize();
-		unique_ptr<float[]> arrayOut(new float[arrayLength]);
-		for (auto i = 0; i < arrayLength; i++) {
-			arrayOut[i] = 1;
-		}
-		return arrayOut;
-	}
 	return methods[name];
 };
 
@@ -217,20 +154,23 @@ auto detectGPU = [] () {
 		cout << " No devices found. Check OpenCL installation!\n";
 		exit(1);
 	}
-	return all_devices[0].getInfo<CL_DEVICE_NAME>();
+	cout << "Using openCL with GPU: " << all_devices[0].getInfo<CL_DEVICE_NAME>() << endl;
 };
 
-auto printSystemInfo = [] {
+auto detectCPU = [] (auto print) {
+	LARGE_INTEGER lpFrequency;
+	SYSTEM_INFO siSysInfo;
 	try {
-		LARGE_INTEGER lpFrequency;
 		QueryPerformanceFrequency(&lpFrequency);
-		SYSTEM_INFO siSysInfo;
 		GetSystemInfo(&siSysInfo);
-		cout << "Using CPU: " << siSysInfo.dwNumberOfProcessors << " cores at about " << float(lpFrequency.QuadPart) / 1'000'000 << " Ghz" << endl;
-		cout << "Using openCL with GPU: " << detectGPU() << endl;
+		
 	} catch (exception const& e) {
 		cout << "Error: " << e.what() << '\n';
 	}
+	if (print) {
+		cout << "Using CPU: " << siSysInfo.dwNumberOfProcessors << " cores at about " << float(lpFrequency.QuadPart) / 1'000'000 << " Ghz" << endl;
+	}
+	return float(lpFrequency.QuadPart);
 };
 
 auto initializeRandomArray = [](auto arrayLength, auto maxRandomValue) {
@@ -246,35 +186,63 @@ auto calculateTime = [](auto name, auto &sumFunction, auto &array1, auto &array2
 	auto output = sumFunction(array1, array2, arrayLength);
 	auto end = steady_clock::now();
 	if (debug) {
-		cout << name << " function done in " << duration_cast<microseconds>(end - begin).count() << " ns. " << endl;
+		cout << name << " function done in " << duration_cast<nanoseconds>(end - begin).count() << " ns. " << endl;
 		cout << array1[0] << " + " << array2[0] << " = " << output[0] << endl;
 	}
-	return duration_cast<microseconds>(end - begin).count();
+	return duration_cast<nanoseconds>(end - begin).count();
 };
 
-
-
 int main() {
-	auto arrayLength = 1048576, randomValueMax = 1'000;
-	cout << "Generating array with " << arrayLength << " elements ...";
-	auto arrayA = initializeRandomArray(arrayLength, randomValueMax);
-	cout << " half way ... ";
-	auto arrayB = initializeRandomArray(arrayLength, randomValueMax);
-	cout << " done ! " << endl;
-	printSystemInfo();
-	vector<string> methods = { "classic", "autoparallel", "autovector", "openmp", "simd", "threads", "openCL" };
+	detectGPU();
+	detectCPU(true);
+	vector<int> lengths = { 4, 16, 128, 1'024, 16'384, 131'072, 1'048'576, 16'777'216, 134'217'728 };
+	std::cout << "Will run " << lengths.size() << " times for a total of +/- 10 minutes. Do not exit." << endl;
+	json jsontiny, jsonbig;
 
-	for_each(methods.begin(), methods.end(), [&arrayA, &arrayB, &arrayLength](auto method) {
-		vector<long long> measurements;
-		for (auto i = 0; i < 1; i++) {
-			measurements.push_back(calculateTime(method, add(method), arrayA, arrayB, arrayLength));
-		}
-		sort(measurements.begin(), measurements.end());
-		auto median = measurements[measurements.size() / 2];
-		auto average = accumulate(measurements.begin(), measurements.end(), .0) / measurements.size();
-		cout << method << ":\naverage: " << average << "\t median: " << median << "\t min: " << measurements.at(0) << "\t max: " << measurements.at(measurements.size() - 1) << endl;
-	});
-	WINPAUSE;
+	for (auto l = 0; l < lengths.size(); l++) {
+		auto arrayLength = lengths[l], randomValueMax = 1'000;
+		std::cout << "Preparing for " << arrayLength << " elements ...";
+		auto arrayA = initializeRandomArray(arrayLength, randomValueMax);
+		std::cout << " half way ... ";
+		auto arrayB = initializeRandomArray(arrayLength, randomValueMax);
+		std::cout << " done, computation starts now ! " << endl;
+
+		vector<string> methods = { "classic", "autoparallel", "autovector", "openmp", "simd", "threads", "openCL" };
+		for_each(methods.begin(), methods.end(), [&arrayA, &arrayB, &arrayLength, &jsontiny](auto method) {
+			vector<long long> measurements;
+			for (auto i = 0; i < 100; i++) {
+				measurements.push_back(calculateTime(method, add(method), arrayA, arrayB, arrayLength));
+			}
+			sort(measurements.begin(), measurements.end());
+			auto median = measurements[measurements.size() / 2];
+			auto average = accumulate(measurements.begin(), measurements.end(), .0) / measurements.size();
+			auto GoperationsPerSecond = arrayLength / average;
+			auto Gbandwith = sizeof(float) * 3 * arrayLength / average;
+			auto ticks = average * detectCPU(false) * 1'000;
+			cout << "> ";
+			cout << method << ": " << GoperationsPerSecond << " GO/s" << endl;
+			jsontiny[method] = { 
+				{ "average", average }, 
+				{ "median", median}, 
+				{ "minimum", measurements.at(0) }, 
+				{ "maximum", measurements.at(measurements.size() - 1) },
+				{ "operationsPerSecond", GoperationsPerSecond },
+				{ "bandwith", Gbandwith },
+				{ "ticks", ticks }
+			};
+		});
+
+		jsonbig[to_string(lengths[l])] = jsontiny;
+		std::cout << "Computation ended " << (l + 1) << "/" << lengths.size() << " are done." << endl;
+		delete[] arrayA;
+		delete[] arrayB;
+	}
+
+	std::ofstream o("logsfordugagjin.json");
+	o << std::setw(4) << jsonbig << std::endl;
+	std::cout << "Program ended and data saved. You can safely exit the program by pressing enter." << endl;
+
+	cin.get();
 	return 0;
 }
 
